a3d5538d79ebcf8692083e7699e83a84a7d3443c
codeshark-lucifer <codeshark.dev@gmail.com>
2025-12-23 22:34:49 +0700

    Update Ignore file

--- src/engine.cpp
+++ src/engine.cpp
@@ -8,24 +8,24 @@ Engine::Engine()
 {
     platform = std::make_shared<Platform>(SCREEN_WIDTH, SCREEN_HEIGHT, APPLICATION_TITLE);
     scene = std::make_shared<Scene>(SCREEN_WIDTH, SCREEN_HEIGHT, "SampleScene");
-    platform->callback = [this](int w, int h) {
+    platform->callback = [this](int w, int h)
+    {
         scene->OnResize(w, h);
     };
     std::cout << "Initialize Engine.\n";
-    
+
     auto camera = scene->AddEntity(std::make_shared<Entity>("Main Camera"), scene);
     camera->AddComponent<Camera>(SCREEN_HEIGHT, SCREEN_HEIGHT);
     camera->position = glm::vec3(0.0f, 0.0f, 5.0f);
 
     // auto cube = Model::Load("assets/models/cube.fbx", scene);
-    auto cube = Model::Load("D:\\Programing Education\\olia - engine\\assets\\models\\octahedron-sharpe.fbx", scene);
+    auto cube = Model::Load("assets/models/octahedron-sharpe.fbx", scene);
     scene->AddEntity(cube, scene);
-
-    std::cout << "SetUp Engine.\n";
 }
 
 void Engine::Run()
 {
+    std::cout << "Running...\n";
     Uint64 now = SDL_GetPerformanceCounter();
     Uint64 last = now;
     double deltaTime = 0.0;
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 int main() {
     try {
         Engine engine; // Engine constructor is called here
-        std::cout << "Engine constructed. Calling Run().\n";
+        std::cout << "Engine Start.\n";
         engine.Run();   // Engine::Run() is called here
-        std::cout << "Engine::Run() finished.\n";
+        std::cout << "Engine ShutDown.\n";
     } catch(const std::exception& e) {
         std::cout << "[Exception]: " << e.what() << "\n";
     }
